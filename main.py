c85b18c9415f9a1e5c8700b64df772a64c86f0b5
henrique <henrique.riccio@gmail.com>
2023-12-01 18:05:21 -0300

    monte carlo

--- main.py
+++ main.py
@@ -12,7 +12,14 @@ def random_walk(n):
 
 
 if __name__ == '__main__':
-    for i in range(25):
-        walk = random_walk(i)
-        print(walk, "Distance from home = ",
-              abs(walk[0]) + abs(walk[1]))
+    number_of_walks = 10000
+    for walk_length in range(1, 31):
+        no_transport = 0  # number of walks 4 or fewer blocks from home
+        for i in range(number_of_walks):
+            (x, y) = random_walk(walk_length)
+            distance = abs(x) + abs(y)
+            if distance <= 4:
+                no_transport += 1
+        no_transport_percentage = float(no_transport) / number_of_walks
+        print("walk size = ", walk_length,
+              " / % of no transport =", 100 * no_transport_percentage)
